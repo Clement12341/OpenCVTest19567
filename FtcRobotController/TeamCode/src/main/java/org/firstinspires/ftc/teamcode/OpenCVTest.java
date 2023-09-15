d5acbd837fa4b5d5d84e1dd8a1e5c3807ec25bc1
Clement12341 <111813679+Clement12341@users.noreply.github.com>
2023-09-14 23:45:19 -0500

    Add files via upload
    Check OpenCvTest for progress

---
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCVTest.java
@@ -0,0 +1,76 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+
+@Autonomous(name="OpenCVTest")
+public class OpenCVTest extends OpMode {
+
+    OpenCvWebcam camera = null;
+
+    @Override
+    public void init() {
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "camera");
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+
+        camera.setPipeline(new examplePipeline());
+
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                camera.startStreaming(640, 360, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+
+            }
+        });
+
+    }
+    @Override
+    public void loop() {
+
+    }
+
+    class examplePipeline extends OpenCvPipeline{
+        Mat YCBCR = new Mat();
+        Mat leftCrop;
+        Mat rightCrop;
+        double leftavgfin;
+        double rightavgfin;
+        Mat outPut = new Mat();
+        Scalar rectColor = new Scalar(255.0, 0.0, 0.0);
+
+        public Mat processFrame(Mat input) {
+
+            Imgproc.cvtColor(input, YCBCR, Imgproc.COLOR_RGB2YCrCb);
+            telemetry.addLine("pipeline running");
+
+            Rect leftRect = new Rect(1, 1, 319, 359);
+            Rect rigthRect = new Rect(320, 1, 319, 359);
+
+            input.copyTo(outPut);
+            Imgproc.rectangle(outPut, leftRect, rectColor, 2);
+            Imgproc.rectangle(outPut, rigthRect, rectColor, 2);
+
+
+
+            return(outPut);
+
+        }
+    }
+}
